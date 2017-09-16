508dcbf3aa55b11a644f0e4dce5135132df96467
Samantha Ramos <samantharamos@samanthas-air.attlocal.net>
2017-09-16 15:23:31 -0500

    backend controller and models set up

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,54 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: [:index, :show, :update]
+
+  # GET /comments
+  def index
+    # @comments = Comment.all
+
+    @comments = Comment.where(post_id: params[:post_id])
+
+    render json: @comments
+  end
+
+  # GET /comments/1
+  def show
+    render json: @comment
+  end
+
+  # POST /comments
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.post_id = params[:post_id]
+
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /comments/1
+  def update
+    if @comment.update(comment_params)
+      render json: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /comments/1
+  # def destroy
+  #   @comment.destroy
+  # end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def comment_params
+      params.require(:comment).permit(:name, :addComment, :post_id)
+    end
+end
